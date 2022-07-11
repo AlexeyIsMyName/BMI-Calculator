85f5bfe7169e769a102f5d1954542026eb922091
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-07-11 23:45:09 +0300

    Made 2 IBActions for 2 Sliders AND 2 IBOutlets for Labels to show data from sliders

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var heightLabel: UILabel!
+    @IBOutlet var weightLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightSlidercChanged(_ sender: UISlider) {
+        heightLabel.text = String(format: "%.1fm", sender.value)
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        weightLabel.text = String(format: "%.0fKg", sender.value)
+    }
+    
 }
 
